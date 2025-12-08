b1028c47adc0ca33d73d10276c769b2f4f1071dd
TK256 <tk256ai@gmail.com>
2025-12-09 00:05:13 +0900

    Initial commit

---
+++ template/template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(int i=0; i<(n); i++)
+
+void solve() {
+    
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
